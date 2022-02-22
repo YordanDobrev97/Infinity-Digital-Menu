7ae6071cfb3319ee854873752f22dadf7df46275
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2022-02-22 16:22:39 +0200

    Add login layout styles

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,87 @@
-import React from 'react'
-import {Text, Button, View} from 'react-native'
+import React, { useState } from 'react'
+import { Text, TouchableOpacity, View, StyleSheet, TextInput } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    image: {
+        marginBottom: 40,
+    },
+
+    inputView: {
+        backgroundColor: "#001f24",
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+
+        alignItems: "center",
+    },
+
+    TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+        color: 'white'
+    },
+
+    forgot_button: {
+        height: 30,
+        marginBottom: 30,
+    },
+
+    loginBtn: {
+        width: "80%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        backgroundColor: "#001f24",
+    },
+    loginText: {
+        color: 'white'
+    }
+})
 
 const Login = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <View>
-            <Text>Login</Text>
-            <Button title='Go back' onPress={() => navigation.navigate('Home')}/>
+        <View style={styles.container}>
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Email"
+                    placeholderTextColor="white"
+                    onChangeText={(email) => setEmail(email)}
+                />
+            </View>
+
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Password"
+                    placeholderTextColor="white"
+                    secureTextEntry={true}
+                    onChangeText={(password) => setPassword(password)}
+                />
+            </View>
+
+            <TouchableOpacity>
+                <Text style={styles.forgot_button}>Forgot Password?</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.loginBtn}>
+                <Text style={styles.loginText}>LOGIN</Text>
+            </TouchableOpacity>
         </View>
     )
 }
