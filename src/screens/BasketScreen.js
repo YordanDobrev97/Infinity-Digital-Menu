20d5d3d5ff3c956fb2bd030a3558af3cbdddd049
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2022-06-11 18:45:48 +0300

    Improve basket layout

--- src/screens/BasketScreen.js
+++ src/screens/BasketScreen.js
@@ -1,296 +1,358 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext } from "react";
 
 import {
-    View,
-    Text,
-    TouchableOpacity,
-    Image,
-    Dimensions,
-    StyleSheet,
-    SafeAreaView
-} from 'react-native'
-import { FlatGrid } from 'react-native-super-grid'
-import Icon from 'react-native-vector-icons/FontAwesome'
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
+import { FlatGrid } from "react-native-super-grid";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-import CartContext from '../context/CartContext'
+import CartContext from "../context/CartContext";
 
-const { width } = Dimensions.get("window")
+const { width } = Dimensions.get("window");
 
 const BasketScreen = ({ navigation }) => {
-    const context = useContext(CartContext)
-    const [products, setProducts] = useState(context.products)
-    const [totalPrice, setTotalPrice] = useState(0)
-    const [successOrder, setSuccessOrder] = useState(false)
-    const [currentHeight, setHeight] = useState(Dimensions.get('screen').height)
-    const [currentWidth, setWidth] = useState(Dimensions.get('screen').width)
+  const context = useContext(CartContext);
+  const [products, setProducts] = useState(context.products);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [successOrder, setSuccessOrder] = useState(false);
+  const [currentHeight, setHeight] = useState(Dimensions.get("screen").height);
+  const [currentWidth, setWidth] = useState(Dimensions.get("screen").width);
 
-    useEffect(() => {
-        updateTotalPrice(products)
-    }, [])
+  useEffect(() => {
+    updateTotalPrice(products);
+  }, []);
 
-    useEffect(() => {
-        if (!isPortrait()) {
-            setWidth('30%')
-            setHeight(width / 4)
-        } else {
-            setWidth('40%')
-            setHeight(width / 3)
-        }
-    }, [currentHeight, currentWidth])
-
-    const onBack = async () => {
-        if (successOrder) {
-            context.setProducts([])
-        }
-        navigation.reset({
-            index: 0,
-            routes: [{ name: 'Home' }]
-        })
+  useEffect(() => {
+    if (!isPortrait()) {
+      setWidth("30%");
+      setHeight(width / 4);
+    } else {
+      setWidth("40%");
+      setHeight(width / 3);
     }
+  }, [currentHeight, currentWidth]);
 
-    const removeProduct = async (id) => {
-        const currentProducts = [...products]
-        const filteredProducts = currentProducts.filter((p) => p.id !== id);
-        await updateTotalPrice(filteredProducts)
-            .then(() => {
-                setProducts(filteredProducts)
-                context.setProducts(filteredProducts)
-            })
-
+  const onBack = async () => {
+    if (successOrder) {
+      context.setProducts([]);
     }
-    const onSendToEmail = async () => {
-        const productsRes = products.map((product) => {
-            return `
+    navigation.reset({
+      index: 0,
+      routes: [{ name: "Home" }],
+    });
+  };
+
+  const removeProduct = async (id) => {
+    const currentProducts = [...products];
+    const filteredProducts = currentProducts.filter((p) => p.id !== id);
+    await updateTotalPrice(filteredProducts).then(() => {
+      setProducts(filteredProducts);
+      context.setProducts(filteredProducts);
+    });
+  };
+  const onSendToEmail = async () => {
+    const productsRes = products.map((product) => {
+      return `
             Продукт: ${product.name}
             Единична цена - ${product.price}лв
             Количество: ${product.count}
-            `
-        })
+            `;
+    });
 
-        fetch('https://digitalmenu-api.herokuapp.com', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                email: 'Nikoletaairbnb1@gmail.com',
-                subject: 'Нова поръчка',
-                text: `
+    fetch("https://digitalmenu-api.herokuapp.com", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: "Nikoletaairbnb1@gmail.com",
+        subject: "Нова поръчка",
+        text: `
                     Поръчани продукти:
-                    ${productsRes.join('\n')}
+                    ${productsRes.join("\n")}
                     Крайна цена: ${totalPrice.toFixed(2)} лв.
-                `
-            })
-        }).then((res) => {
-            if (res) {
-                setSuccessOrder(true)
-            }
-        })
+                `,
+      }),
+    }).then((res) => {
+      if (res) {
+        setSuccessOrder(true);
+      }
+    });
+  };
+
+  Dimensions.addEventListener("change", () => {
+    setHeight(Dimensions.get("screen").height);
+    setWidth(Dimensions.get("screen").width);
+  });
+
+  const isPortrait = () => {
+    const dim = Dimensions.get("screen");
+    return dim.height >= dim.width;
+  };
+
+  const onIncreaseQuantity = async (id) => {
+    const shadowProducts = [...products];
+    const product = shadowProducts.find((p) => p.id === id);
+    product.count++;
+    updateTotalPrice(shadowProducts).then((res) => {
+      setProducts(shadowProducts);
+    });
+  };
+
+  const onDecreaseQuantity = async (id) => {
+    const shadowProducts = [...products];
+    const product = shadowProducts.find((p) => p.id === id);
+    if (product.count > 1) {
+      product.count--;
+      updateTotalPrice(shadowProducts).then(() => {
+        setProducts(shadowProducts);
+      });
     }
+  };
 
-    Dimensions.addEventListener('change', () => {
-        setHeight(Dimensions.get('screen').height)
-        setWidth(Dimensions.get('screen').width)
-    })
+  const updateTotalPrice = async (products) => {
+    let totalPrice = 0;
+    products.forEach((product) => {
+      totalPrice += product.count * Number(product.price);
+    });
+    setTotalPrice(totalPrice);
+  };
 
-    const isPortrait = () => {
-        const dim = Dimensions.get('screen')
-        return dim.height >= dim.width
-    }
-
-    const onIncreaseQuantity = async (id) => {
-        const shadowProducts = [...products]
-        const product = shadowProducts.find((p) => p.id === id)
-        product.count++;
-        updateTotalPrice(shadowProducts)
-            .then((res) => {
-                setProducts(shadowProducts)
-            })
-    }
-
-    const onDecreaseQuantity = async (id) => {
-        const shadowProducts = [...products]
-        const product = shadowProducts.find((p) => p.id === id)
-        if (product.count > 1) {
-            product.count--;
-            updateTotalPrice(shadowProducts)
-                .then(() => {
-                    setProducts(shadowProducts)
-                })
-        }
-    }
-
-    const updateTotalPrice = async (products) => {
-        let totalPrice = 0
-        products.forEach((product) => {
-            totalPrice += product.count * Number(product.price)
-        })
-        setTotalPrice(totalPrice)
-    }
-
-    const renderItem = ({ item }) => {
-        return (
-            <View style={{
-                flex: 1,
-                width: '100%',
-                height: '50%'
-            }}>
-                <View style={{ width: '100%',
-                height: '100%',
-                backgroundColor: 'black', flexDirection:
-                'row', borderBottomWidth: 2,
-                borderColor: "#cccccc",
-                }}>
-                    <Image
-                        resizeMode={"contain"}
-                        style={{
-                            width: '40%',
-                            height: '40%',
-                            padding: 20,
-                            justifyContent: 'center',
-                            alignSelf: 'center'
-                        }}
-                        source={{ uri: item.image }} />
-                    <View
-                        style={{ flex: 1, backgroundColor: 'transparent', padding: 10, justifyContent: "space-between" }}>
-                        <View>
-                            <Text style={{ fontWeight: "bold", color: 'white', fontSize: 17 }}>{item.name}</Text>
-                        </View>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                            <Text style={{ fontWeight: 'bold', color: "#9fd236", fontSize: 17 }}>
-                                {item.count}бр x {Number(item.price).toFixed(2)}лв
-                            </Text>
-                        </View>
-                        <View style={{ flex: 1, flexDirection: 'row' }}>
-                            <TouchableOpacity onPress={() => onIncreaseQuantity(item.id)} style={{
-                                width: 40,
-                                height: 30,
-                                margin: 6,
-                                backgroundColor: '#FE9A28'
-                            }}>
-                                <Text style={{ fontWeight: 'bold', color: "#ffff", textAlign: 'center', fontSize: 20 }}>
-                                    +
-                                </Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity
-                                onPress={() => onDecreaseQuantity(item.id)}
-                                style={{
-                                    width: 40,
-                                    height: 30,
-                                    margin: 6,
-                                    backgroundColor: '#FE9A28'
-                                }}>
-                                <Text style={{ fontWeight: 'bold', color: "#ffff", textAlign: 'center', fontSize: 20 }}>
-                                    -
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <TouchableOpacity onPress={() => removeProduct(item.id)} style={{ backgroundColor: '#dae2e4', padding: 8, borderRadius: 12 }}>
-                                <Text>Премахни</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
-            </View>
-        )
-    }
-
-    if (successOrder) {
-        return (
-            <View style={{
-                backgroundColor: '#23212E',
-                height: '100%',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}>
-                <Text style={{ color: 'white', textAlign: 'center' }}>Вашата поръчка беше изпратена успешно!</Text>
-                <TouchableOpacity
-                    onPress={onBack}
-                    style={{
-                        backgroundColor: 'orange',
-                        width: '40%',
-                        padding: 10,
-                        margin: 8,
-                        borderRadius: 24,
-                    }}>
-                    <Text style={{ textAlign: 'center', color: 'black' }}>Към начална страница</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-
+  const renderItem = ({ item }) => {
     return (
-        <View style={styles.screen}>
-            <Icon.Button
-                style={{ marginTop: 18 }}
-                onPress={onBack}
-                name="arrow-left"
-                backgroundColor="black">
-                <Text style={{ fontSize: 15, color: '#ffff', textAlign: 'center' }}>
-                    Назад
-                </Text>
-            </Icon.Button>
-
-            <View style={styles.summary}>
-                {products && (
-                    <SafeAreaView style={{flex: 1}}>
-                        <FlatGrid
-                        itemDimension={200}
-                        keyExtractor={(item) => item.id}
-                        data={products}
-                        renderItem={renderItem}
-                    />
-                    </SafeAreaView>
-                )}
-            </View>
-
-            <View>
-                <Text style={{ color: '#ffff', fontSize: 20, textAlign: 'center' }}>Крайна цена: {totalPrice.toFixed(2)} лв</Text>
-            </View>
-            <TouchableOpacity style={{
-                backgroundColor: "#303650",
-                width: '100%',
-                alignItems: 'center',
-                padding: 8,
-                borderRadius: 5
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+          height: "50%",
+        }}
+      >
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: "black",
+            flexDirection: "row",
+            borderBottomWidth: 2,
+            borderColor: "#cccccc",
+          }}
+        >
+          <Image
+            resizeMode={"contain"}
+            style={{
+              width: "40%",
+              height: "40%",
+              padding: 20,
+              justifyContent: "center",
+              alignSelf: "center",
             }}
-                onPress={onSendToEmail}>
-                <Text style={{
-                    fontSize: 24,
-                    fontWeight: "bold",
-                    color: 'white'
-                }}>
-                    Поръчай
+            source={{ uri: item.image }}
+          />
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "transparent",
+              padding: 10,
+              justifyContent: "space-between",
+            }}
+          >
+            <View>
+              <Text
+                style={{ fontWeight: "bold", color: "white", fontSize: 17 }}
+              >
+                {item.name}
+              </Text>
+            </View>
+            <View style={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-end'}}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text
+                  style={{ fontWeight: "bold", color: "#9fd236", fontSize: 17 }}
+                >
+                  {item.count}бр x {Number(item.price).toFixed(2)}лв
                 </Text>
-            </TouchableOpacity>
-            <View style={{ height: 20 }} />
+              </View>
+              <View style={{ flex: 1, flexDirection: "row" }}>
+                <TouchableOpacity
+                  onPress={() => onIncreaseQuantity(item.id)}
+                  style={{
+                    width: 40,
+                    height: 30,
+                    margin: 6,
+                    backgroundColor: "#FE9A28",
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontWeight: "bold",
+                      color: "#ffff",
+                      textAlign: "center",
+                      fontSize: 20,
+                    }}
+                  >
+                    +
+                  </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={() => onDecreaseQuantity(item.id)}
+                  style={{
+                    width: 40,
+                    height: 30,
+                    margin: 6,
+                    backgroundColor: "#FE9A28",
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontWeight: "bold",
+                      color: "#ffff",
+                      textAlign: "center",
+                      fontSize: 20,
+                    }}
+                  >
+                    -
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <TouchableOpacity
+                onPress={() => removeProduct(item.id)}
+                style={{
+                  backgroundColor: "#dae2e4",
+                  padding: 8,
+                  borderRadius: 12,
+                }}
+              >
+                <Text>Премахни</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-    )
-}
+      </View>
+    );
+  };
+
+  if (successOrder) {
+    return (
+      <View
+        style={{
+          backgroundColor: "#23212E",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "white", textAlign: "center" }}>
+          Вашата поръчка беше изпратена успешно!
+        </Text>
+        <TouchableOpacity
+          onPress={onBack}
+          style={{
+            backgroundColor: "orange",
+            width: "40%",
+            padding: 10,
+            margin: 8,
+            borderRadius: 24,
+          }}
+        >
+          <Text style={{ textAlign: "center", color: "black" }}>
+            Към начална страница
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Icon.Button
+        style={{ marginTop: 18 }}
+        onPress={onBack}
+        name="arrow-left"
+        backgroundColor="black"
+      >
+        <Text style={{ fontSize: 15, color: "#ffff", textAlign: "center" }}>
+          Назад
+        </Text>
+      </Icon.Button>
+
+      <View style={styles.summary}>
+        {products && (
+          <SafeAreaView style={{ flex: 1 }}>
+            <FlatGrid
+              itemDimension={200}
+              keyExtractor={(item) => item.id}
+              data={products}
+              renderItem={renderItem}
+            />
+          </SafeAreaView>
+        )}
+      </View>
+
+      <View>
+        <Text style={{ color: "#ffff", fontSize: 20, textAlign: "center" }}>
+          Крайна цена: {totalPrice.toFixed(2)} лв
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#303650",
+          width: "100%",
+          alignItems: "center",
+          padding: 8,
+          borderRadius: 5,
+        }}
+        onPress={onSendToEmail}
+      >
+        <Text
+          style={{
+            fontSize: 24,
+            fontWeight: "bold",
+            color: "white",
+          }}
+        >
+          Поръчай
+        </Text>
+      </TouchableOpacity>
+      <View style={{ height: 20 }} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        //margin: 20,
-        backgroundColor: 'black',
-        color: '#C6C5CB',
-        height: '100%',
-    },
-    summary: {
-        flexDirection: 'row',
-        //alignItems: 'top',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-        naxHeight: '60%',
-        //height: '60%',
-        flex: 1
-    },
-    summaryText: {
-        fontFamily: 'open-sans-bold',
-        fontSize: 18
-    },
-    amount: {
-        color: 'blue'
-    }
+  screen: {
+    //margin: 20,
+    backgroundColor: "black",
+    color: "#C6C5CB",
+    height: "100%",
+  },
+  summary: {
+    flexDirection: "row",
+    //alignItems: 'top',
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    naxHeight: "60%",
+    //height: '60%',
+    flex: 1,
+  },
+  summaryText: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
+  },
+  amount: {
+    color: "blue",
+  },
 });
 
-export default BasketScreen
+export default BasketScreen;
